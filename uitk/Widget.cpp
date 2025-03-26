eightbrains/uitk

commit 0066eb2ca6b7b25abfb6194b72147d8eb185d536

Fix incorrect point conversion when going through a ScrollView (manifesting in Button not firing onClicked)

uitk/Widget.cpp
@@ -375,7 +375,7 @@ Point Widget::convertToLocalFromWindow(const Point& windowPt) const
     }
     const Widget *w = this;
     while (w->mImpl->parent) {
-        localPt -= w->frame().upperLeft();
+        localPt = w->convertToLocalFromParent(localPt);
         w = w->mImpl->parent;
     }
     return localPt;
@@ -389,10 +389,20 @@ Point Widget::convertToWindowFromLocal(const Point& localPt) const
     }
     const Widget *w = this;
     while (w && w->mImpl->parent) {
-        windowPt += w->frame().upperLeft();
+        windowPt = w->convertToLocalFromParent(windowPt);
         w = w->mImpl->parent;
     }
     return windowPt;
+}
+
+Point Widget::convertToLocalFromParent(const Point& parentPt) const
+{
+    return parentPt - frame().upperLeft();
+}
+
+Point Widget::convertToParentFromLocal(const Point& localPt) const
+{
+    return localPt + frame().upperLeft();
 }
 
 void Widget::setNeedsDraw()

uitk/ScrollView.cpp
@@ -226,6 +226,16 @@ Point ScrollView::scrollPosition() const
     return Point(-b.x, -b.y);
 }
 
+Point ScrollView::convertToLocalFromParent(const Point& parentPt) const
+{
+    return parentPt - bounds().upperLeft() - frame().upperLeft();
+}
+
+Point ScrollView::convertToParentFromLocal(const Point& localPt) const
+{
+    return localPt + bounds().upperLeft() + frame().upperLeft();
+}
+
 AccessibilityInfo ScrollView::accessibilityInfo()
 {
     auto info = Super::accessibilityInfo();

uitk/ScrollView.h
@@ -56,6 +56,9 @@ public:
     /// same coordinate system that scrollTo() uses.
     Point scrollPosition() const;
 
+    Point convertToLocalFromParent(const Point& parentPt) const override;
+    Point convertToParentFromLocal(const Point& localPt) const override;
+
     AccessibilityInfo accessibilityInfo() override;
 
     Size preferredSize(const LayoutContext& context) const override;

uitk/Widget.h
@@ -131,6 +131,9 @@ public:
     Point convertToLocalFromWindow(const Point& windowPt) const;
     Point convertToWindowFromLocal(const Point& localPt) const;
 
+    virtual Point convertToLocalFromParent(const Point& parentPt) const;
+    virtual Point convertToParentFromLocal(const Point& localPt) const;
+
     bool focused() const;
     void resignKeyFocus() const;
 

commit beef721793d552dcc48a5e123783a465e54d9370

Fix ComboBox menu appearing invisibly (regression from 0066eb2)

uitk/Widget.cpp
@@ -389,7 +389,7 @@ Point Widget::convertToWindowFromLocal(const Point& localPt) const
     }
     const Widget *w = this;
     while (w && w->mImpl->parent) {
-        windowPt = w->convertToLocalFromParent(windowPt);
+        windowPt = w->convertToParentFromLocal(windowPt);
         w = w->mImpl->parent;
     }
     return windowPt;